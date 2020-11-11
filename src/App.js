9fab40b377839daf68dcf5026809f6960d3f5ace
Hunter Kantner <hunterkantner@gmail.com>
2020-11-10 18:31:29 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from "./pages/main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Router >
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
