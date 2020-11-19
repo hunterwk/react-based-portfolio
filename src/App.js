9e799e973de98ac4219000dee79efc32a49ee0f0
Hunter Kantner <hunterkantner@gmail.com>
2020-11-19 00:26:03 -0800

    added container class

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Main from "./pages/main";
 import Contact from "./pages/contact";
+import AboutMe from "./pages/aboutme";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -9,12 +10,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="container mx-auto">
       <Header />
       <Router >
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/contact" component={Contact} />
+          <Route exact path="/aboutme" component={AboutMe} />
         </Switch>
       </Router>
       <Footer />
---
+++ src/pages/aboutme.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutMe() {
+    return (
+        <div>
+            <p>I am a former bartender turned web developer.  I really enjoy using react and learning something new everyday.</p>
+        </div>
+    )
+
+}
+
+export default AboutMe;
